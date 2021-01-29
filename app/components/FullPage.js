e2f9d63bbd1c4396b1507b4e79b134db3a3713b1
noha <nono@MacBook-Pro-de-Noha.local>
2021-01-29 15:08:19 +0100

    feat(fullpage): Add sections in wrapper

--- app/components/FullPage.js
+++ app/components/FullPage.js
@@ -4,12 +4,22 @@ import ReactFullpage from '@fullpage/react-fullpage';
 export default function FullPage() {
     return (
         <ReactFullpage
-        scrollingSpeed = {1000} /* Options here */
     
         render={({ state, fullpageApi }) => {
           return (
             <ReactFullpage.Wrapper>
-
+                <section className="section home-section">
+                    <h1>Home</h1>
+                </section>
+                <section className="section about-section">
+                    <h1>About</h1>
+                </section>
+                <section className="section portfolio-section">
+                    <h1>Portfolio</h1>
+                </section>
+                <section className="section contacts-section">
+                    <h1>Contacts</h1>
+                </section>
             </ReactFullpage.Wrapper>
           );
         }}

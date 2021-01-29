563b555a8b2bbae632d6ebcb5a180769d9c6595e
noha <nono@MacBook-Pro-de-Noha.local>
2021-01-29 20:59:44 +0100

    feat(app): Create a request for get projects data

--- app/components/sections/PortfolioSection.js
+++ app/components/sections/PortfolioSection.js
@@ -1,7 +1,37 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 export default function PortfolioSection() {
+
+    const [projects, setProjects] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(false)
+
+    const fetchData = async() => {
+        await axios.get('http://localhost:1337/projects')
+            .then(res => {
+                setError(false)
+                setProjects(prevState => [...prevState, ...res.data])
+            })
+            .catch(() => {
+                setError(true)
+            })
+            .finally(() => {
+                setLoading(false)
+            })
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
     return (
         <section className="section portfolio-section">
-            <h1>Portfolio</h1>
+            <ul>
+                {projects.map((project, key) => (
+                    <li key={key}>{project.title}</li>
+                ))}
+            </ul>
         </section>
     )
 }

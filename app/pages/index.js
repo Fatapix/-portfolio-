cd6722b986f0593d047fcca4574c027ffe0b862f
noha <nono@MacBook-Pro-de-Noha.local>
2021-01-29 11:09:47 +0100

    config(app): Configure Sass and create default files

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,8 +1,9 @@
 import Layout from "../components/Layout";
+
 export default function Home() {
   return (
     <Layout page="Portfolio - KHIRAT Noha">
-      
+      <h1>Portfolio</h1>
     </Layout>
   )
 }

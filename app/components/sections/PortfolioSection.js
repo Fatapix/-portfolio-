35f0bc358d0346c78310214acc9d50ee77523e89
noha <nono@MacBook-Pro-de-Noha.local>
2021-01-29 23:02:21 +0100

    feat(portfolio-section): Install and init Tilt JS

--- app/components/sections/PortfolioSection.js
+++ app/components/sections/PortfolioSection.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import Tilt from 'react-tilt';
 
 export default function PortfolioSection() {
 
+    const serverAddresse = "http://localhost:1337"
+    
     const [projects, setProjects] = useState([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
@@ -27,36 +30,22 @@ export default function PortfolioSection() {
 
     return (
         <section className="section portfolio-section">
-            {/* <ul className="projects-container">
-                {projects.map((project, key) => (
-                    <li className="item" key={key}>{project.title}</li>
-                ))}
-            </ul> */}
             <div className="projects-container">
-                <div className="card">
-                    <div className="card-img">
-                        <img src="/img/design.jpg"></img>
-                    </div>
-                    <div className="card-content">
-                        <h2>Website</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                            tempor incididunt ut labore et dolore magna aliqua.
-                        </p>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="card-img">
-                        <img src="/img/design.jpg"></img>
-                    </div>
-                    <div className="card-content">
-                        <h2>Website</h2>
-                        <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
-                            tempor incididunt ut labore et dolore magna aliqua.
-                        </p>
-                    </div>
-                </div>
+                {projects.map((project, key) => (
+                    <Tilt className="tilt" options={{ max : 25 }}>
+                        <div className="card">
+                            <div className="card-img">
+                                <img src={serverAddresse + project.screen.url}></img>
+                            </div>
+                            <div className="card-content">
+                                <h2>{project.title}</h2>
+                                <p>
+                                    {project.content}
+                                </p>
+                            </div>
+                        </div>
+                    </Tilt>
+                ))}
             </div>
         </section>
     )

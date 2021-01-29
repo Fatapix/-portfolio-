ceb4160e3166649ebaf726630f451a937b944457
noha <nono@MacBook-Pro-de-Noha.local>
2021-01-29 14:14:23 +0100

    refactor(index): Delete h1 for test

--- app/pages/index.js
+++ app/pages/index.js
@@ -2,8 +2,6 @@ import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <Layout page="Portfolio - KHIRAT Noha">
-      <h1>Portfolio</h1>
-    </Layout>
+    <Layout page="Portfolio - KHIRAT Noha"></Layout>
   )
 }

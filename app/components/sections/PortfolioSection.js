eb89c32b55a970d4d28f5174cbd4d0fc4840bbf6
noha <nono@MacBook-Pro-de-Noha.local>
2021-01-29 23:15:12 +0100

    refactor(portfolio-section)

--- app/components/sections/PortfolioSection.js
+++ app/components/sections/PortfolioSection.js
@@ -3,15 +3,14 @@ import { useEffect, useState } from 'react';
 import Tilt from 'react-tilt';
 
 export default function PortfolioSection() {
-
     const serverAddresse = "http://localhost:1337"
-    
+
     const [projects, setProjects] = useState([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(false)
 
     const fetchData = async() => {
-        await axios.get('http://localhost:1337/projects')
+        await axios.get(serverAddresse +'/projects')
             .then(res => {
                 setError(false)
                 setProjects(prevState => [...prevState, ...res.data])

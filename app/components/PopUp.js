6887a2e020d60a1da44747f3e9180297b49b1804
noha <nono@MacBook-Pro-de-Noha.local>
2021-01-31 00:38:30 +0100

    feat(portfolio-section): Add files

---
+++ app/components/PopUp.js
@@ -0,0 +1,19 @@
+export default function PopUp({ data }) {
+    return (
+        <div className="pop-up-container">            
+            <div className="pop-up">
+                <div className="carousel"></div>
+                <div className="content">
+                    <h2>Portfolio</h2>
+                    <p>
+                        On the other hand, we denounce with righteous indignation and dislike men who are so 
+                        beguiled and demoralized by the charms of pleasure of the moment, 
+                        so blinded by desire, that they cannot foresee.
+                    </p>
+                </div>
+            </div>
+
+            <div className="background-filter"></div>
+        </div>
+    )
+}
